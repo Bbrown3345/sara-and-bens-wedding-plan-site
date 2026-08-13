fed75cc6f1211bb882d5bf2db62f2252e8d14b53
Bbrown3345 <BenLeeBrown@hotmail.com>
2026-08-12 22:26:04 -0400

    Add files via upload

---
+++ js/schedule-data.js
@@ -0,0 +1,434 @@
+var SCHEDULE_DATA = [
+  {
+    "time": "07:45",
+    "event": "Makeup",
+    "owner": "Maddi (Makeup)",
+    "people": "Logan C (B1)"
+  },
+  {
+    "time": "08:00",
+    "event": "Hair",
+    "owner": "Jul (Hair)",
+    "people": "Logan C (B1)"
+  },
+  {
+    "time": "08:00",
+    "event": "Order Breakfast",
+    "owner": "Casey (BM 1), Garrett (BM 2)",
+    "people": ""
+  },
+  {
+    "time": "08:30",
+    "event": "Makeup",
+    "owner": "Maddi (Makeup)",
+    "people": "Jenni (B2)"
+  },
+  {
+    "time": "08:45",
+    "event": "Hair",
+    "owner": "Jul (Hair)",
+    "people": "Jenni (B2)"
+  },
+  {
+    "time": "09:15",
+    "event": "Makeup",
+    "owner": "Maddi (Makeup)",
+    "people": "Ariah (B3)"
+  },
+  {
+    "time": "09:30",
+    "event": "Hair",
+    "owner": "Jul (Hair)",
+    "people": "Ariah (B3)"
+  },
+  {
+    "time": "10:00",
+    "event": "Makeup",
+    "owner": "Maddi (Makeup)",
+    "people": "Megan (B4)"
+  },
+  {
+    "time": "10:15",
+    "event": "Hair",
+    "owner": "Jul (Hair)",
+    "people": "Megan (B4)"
+  },
+  {
+    "time": "10:45",
+    "event": "Makeup",
+    "owner": "Maddi (Makeup)",
+    "people": "Cheyanne (MOH)"
+  },
+  {
+    "time": "11:00",
+    "event": "Hair",
+    "owner": "Jul (Hair)",
+    "people": "Sara"
+  },
+  {
+    "time": "11:30",
+    "event": "Makeup",
+    "owner": "Maddi (Makeup)",
+    "people": "Amber (B5)"
+  },
+  {
+    "time": "11:30",
+    "event": "Arrival",
+    "owner": "Brittany (Photography)",
+    "people": ""
+  },
+  {
+    "time": "11:45",
+    "event": "BUFFER",
+    "owner": "",
+    "people": ""
+  },
+  {
+    "time": "12:00",
+    "event": "Flat/Detail (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": ""
+  },
+  {
+    "time": "12:00",
+    "event": "Order lunch",
+    "owner": "Casey (BM 1), Garrett (BM 2)",
+    "people": ""
+  },
+  {
+    "time": "12:15",
+    "event": "Makeup",
+    "owner": "Maddi (Makeup)",
+    "people": "Sara"
+  },
+  {
+    "time": "12:15",
+    "event": "Hair",
+    "owner": "Jul (Hair)",
+    "people": "Cheyanne (MOH)"
+  },
+  {
+    "time": "12:30",
+    "event": "Arrival",
+    "owner": "Leiter's (Catering)",
+    "people": ""
+  },
+  {
+    "time": "12:30",
+    "event": "Getting Ready (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": "Sara, Cheyanne (MOH), Bridesmaids (All)"
+  },
+  {
+    "time": "12:45",
+    "event": "Getting Ready (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": "Ben, Casey (BM 1), Garrett (BM 2), Groomsmen (All)"
+  },
+  {
+    "time": "13:00",
+    "event": "Arrival / Set Up",
+    "owner": "Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "13:00",
+    "event": "Getting Ready (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": "Sara, Cheyanne (MOH), Bridesmaids (All)"
+  },
+  {
+    "time": "13:00",
+    "event": "Hair",
+    "owner": "Jul (Hair)",
+    "people": "Pam (MOG), Emilia (FG 1), Veronica (FG 2)"
+  },
+  {
+    "time": "13:00",
+    "event": "Get Dressed",
+    "owner": "Sara",
+    "people": "Cheyanne (MOH), Robin (MOB)"
+  },
+  {
+    "time": "13:15",
+    "event": "Portraits (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": "Ben, Casey (BM 1), Garrett (BM 2), Groomsmen (All)"
+  },
+  {
+    "time": "13:30",
+    "event": "Portraits (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": "Sara, Cheyanne (MOH), Bridesmaids (All)"
+  },
+  {
+    "time": "13:30",
+    "event": "Hair",
+    "owner": "Jul (Hair)",
+    "people": "Donna (SMOB)"
+  },
+  {
+    "time": "13:30",
+    "event": "BUFFER",
+    "owner": "",
+    "people": ""
+  },
+  {
+    "time": "13:50",
+    "event": "First Look (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": "Sara, Ben"
+  },
+  {
+    "time": "14:00",
+    "event": "Arrival / Set Up",
+    "owner": "Mel (Bartender)",
+    "people": ""
+  },
+  {
+    "time": "14:00",
+    "event": "Portraits (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": "Sara, Ben"
+  },
+  {
+    "time": "14:15",
+    "event": "Venue/Area (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": ""
+  },
+  {
+    "time": "14:30",
+    "event": "Reception Space (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": ""
+  },
+  {
+    "time": "14:30",
+    "event": "Freshen Up/Go Inside",
+    "owner": "Cait (Coordinator), Emma (Coordinators)",
+    "people": "Sara, Ben, Cheyanne (MOH), Bridesmaids (All), Casey (BM 1), Garrett (BM 2), Groomsmen (All)"
+  },
+  {
+    "time": "14:40",
+    "event": "Start Wedding Party Assemble",
+    "owner": "Cait (Coordinator), Emma (Coordinators)",
+    "people": "Jason P (Pastor), Sara, Ben, Cheyanne (MOH), Bridesmaids (All), Casey (BM 1), Garrett (BM 2), Groomsmen (All), Emilia (FG 1), Veronica (FG 2), Finn (RB 1), Jett (RB 2), Robin (MOB), Edward (SFOB), Brad (FOB), Donna (SMOB), Pam (MOG), Lee (FOG), Cody, Ashlee"
+  },
+  {
+    "time": "14:45",
+    "event": "BUFFER",
+    "owner": "",
+    "people": ""
+  },
+  {
+    "time": "14:45",
+    "event": "Pre-Ceremony (Music)",
+    "owner": "Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "14:45",
+    "event": "Arrival Window (Start)",
+    "owner": "Guests",
+    "people": ""
+  },
+  {
+    "time": "14:50",
+    "event": "Bridal Party Assemble Deadline",
+    "owner": "Cait (Coordinator), Emma (Coordinator)",
+    "people": "Jason P (Pastor), Sara, Ben, Cheyanne (MOH), Bridesmaids (All), Casey (BM 1), Garrett (BM 2), Groomsmen (All), Emilia (FG 1), Veronica (FG 2), Finn (RB 1), Jett (RB 2), Robin (MOB), Edward (SFOB), Brad (FOB), Donna (SMOB), Pam (MOG), Lee (FOG), Cody, Ashlee"
+  },
+  {
+    "time": "15:05",
+    "event": "Bridal Party Lined Up",
+    "owner": "Cait (Coordinator), Emma (Coordinator)",
+    "people": "Jason P (Pastor), Sara, Ben, Cheyanne (MOH), Bridesmaids (All), Casey (BM 1), Garrett (BM 2), Groomsmen (All), Emilia (FG 1), Veronica (FG 2), Finn (RB 1), Jett (RB 2), Robin (MOB), Edward (SFOB), Brad (FOB), Donna (SMOB), Pam (MOG), Lee (FOG), Cody, Ashlee"
+  },
+  {
+    "time": "15:15",
+    "event": "Arrival Window (End)",
+    "owner": "",
+    "people": ""
+  },
+  {
+    "time": "15:15",
+    "event": "Ceremony Start",
+    "owner": "Jason P (Pastor)",
+    "people": "Guests"
+  },
+  {
+    "time": "15:15",
+    "event": "Ceremony (Music)",
+    "owner": "Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "15:15",
+    "event": "Ceremony (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": ""
+  },
+  {
+    "time": "15:45",
+    "event": "Ceremony End / Announcements",
+    "owner": "Jason P (Pastor)",
+    "people": ""
+  },
+  {
+    "time": "15:45",
+    "event": "Cocktail Hour Start",
+    "owner": "Leiter's (Catering), Mel (Bartender)",
+    "people": "Guests"
+  },
+  {
+    "time": "15:45",
+    "event": "Cocktail Hour (Music)",
+    "owner": "Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "15:45",
+    "event": "Immediate Family (Photos)",
+    "owner": "Brittany (Photography), Cait (Coordinator), Emma (Coordinators)",
+    "people": "Sara, Ben, Cheyanne (MOH), Emilia (FG 1), Veronica (FG 2), Finn (RB 1), Jett (RB 2), Robin (MOB), Edward (SFOB), Brad (FOB), Donna (SMOB), Pam (MOG), Lee (FOG), Cody, Ashlee, Ethan"
+  },
+  {
+    "time": "16:00",
+    "event": "Full Wedding Party (Photos)",
+    "owner": "Brittany (Photography), Cait (Coordinator), Emma (Coordinators)",
+    "people": "Sara, Ben, Bridesmaids (All), Groomsmen (All)"
+  },
+  {
+    "time": "16:30",
+    "event": "BUFFER",
+    "owner": "",
+    "people": ""
+  },
+  {
+    "time": "16:55",
+    "event": "Transition to Reception",
+    "owner": "Mike (DJ)",
+    "people": "Guests"
+  },
+  {
+    "time": "17:00",
+    "event": "Reception Start",
+    "owner": "Leiter's (Catering), Mel (Bartender)",
+    "people": "Guests"
+  },
+  {
+    "time": "17:00",
+    "event": "Grand Entrances",
+    "owner": "Mike (DJ)",
+    "people": "Sara, Ben, Bridesmaids (All), Groomsmen (All)"
+  },
+  {
+    "time": "17:05",
+    "event": "First Dance",
+    "owner": "Mike (DJ)",
+    "people": "Sara, Ben"
+  },
+  {
+    "time": "17:15",
+    "event": "Welcome Toast/Blessing",
+    "owner": "Mike (DJ)",
+    "people": "Lee (FOG)"
+  },
+  {
+    "time": "17:15",
+    "event": "Dinner Service (Start)",
+    "owner": "Leiter's (Catering), Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "17:45",
+    "event": "Toasts",
+    "owner": "Mike (DJ)",
+    "people": "Cheyanne (MOH), Casey (BM 1), Garrett (BM 2)"
+  },
+  {
+    "time": "18:00",
+    "event": "Last Call (Dinner)",
+    "owner": "Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "18:15",
+    "event": "Dinnner Serivce (End)",
+    "owner": "Leiter's (Catering)",
+    "people": ""
+  },
+  {
+    "time": "18:15",
+    "event": "Father/Daughter Dance",
+    "owner": "Mike (DJ)",
+    "people": "Sara, Brad (FOB)"
+  },
+  {
+    "time": "18:20",
+    "event": "Mother/Son Dance",
+    "owner": "Mike (DJ)",
+    "people": "Ben, Pam (MOG)"
+  },
+  {
+    "time": "18:25",
+    "event": "Cake Cutting",
+    "owner": "Mike (DJ)",
+    "people": "Sara, Ben"
+  },
+  {
+    "time": "18:30",
+    "event": "Dancefloor (Start)",
+    "owner": "Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "18:45",
+    "event": "Sunset (Photos)",
+    "owner": "Brittany (Photography)",
+    "people": "Sara, Ben"
+  },
+  {
+    "time": "18:45",
+    "event": "Anniversary Dance",
+    "owner": "Mike (DJ)",
+    "people": "Sara, Ben, Guests"
+  },
+  {
+    "time": "20:00",
+    "event": "Grand Exit (Photos)",
+    "owner": "Brittany (Photography), Mike (DJ)",
+    "people": "Sara, Ben, Guests"
+  },
+  {
+    "time": "20:30",
+    "event": "Last Call (Drinks)",
+    "owner": "Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "20:45",
+    "event": "Bar Closes",
+    "owner": "Mel (Bartender)",
+    "people": ""
+  },
+  {
+    "time": "21:55",
+    "event": "Last Song",
+    "owner": "Mike (DJ)",
+    "people": ""
+  },
+  {
+    "time": "22:00",
+    "event": "Noise Ordenance",
+    "owner": "Sara, Ben",
+    "people": ""
+  },
+  {
+    "time": "22:00",
+    "event": "As Needed Cleanup",
+    "owner": "",
+    "people": ""
+  }
+];
